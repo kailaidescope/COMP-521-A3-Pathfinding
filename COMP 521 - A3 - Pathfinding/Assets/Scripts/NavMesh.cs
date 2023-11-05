76ddb30832675995896518e0e5bc8271c341d777
kai turanski <kai.turanski@mail.mcgill.ca>
2023-11-05 00:16:12 -0400

    Made NavMesh Generator

--- COMP 521 - A3 - Pathfinding/Assets/Scripts/NavMesh.cs
+++ COMP 521 - A3 - Pathfinding/Assets/Scripts/NavMesh.cs
@@ -1,22 +1,118 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.ProBuilder;
 
 public class NavMesh : MonoBehaviour
 {
-    [SerializeField] GameObject referenceShape;
+    [SerializeField] Vector2[] referencePlaneCorners = new Vector2[2];
+    [SerializeField] float height;
 
     private List<List<Partition>> partitions;
+    float xStorageOffset;
+    float yStorageOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateNavMesh();
+
+        foreach (List<Partition> ps in partitions)
+        {
+            foreach (Partition p in ps)
+            {
+                p.Draw(Color.yellow, Color.red);
+            }
+        }
+
+    }
+
+    void GenerateNavMesh()
+    {
+        partitions = new List<List<Partition>>();
+
+        float minx = Mathf.Min(referencePlaneCorners[0].x, referencePlaneCorners[1].x);
+        float maxx = Mathf.Max(referencePlaneCorners[0].x, referencePlaneCorners[1].x) - 1;
+        float miny = Mathf.Min(referencePlaneCorners[0].y, referencePlaneCorners[1].y);
+        float maxy = Mathf.Max(referencePlaneCorners[0].y, referencePlaneCorners[1].y) - 1;
+
+        xStorageOffset = -minx - 0.5f;
+        yStorageOffset = -miny - 0.5f;
+
+        for (float x = minx; x <= maxx; x++)
+        {
+            partitions.Add(new List<Partition>());
+
+            for (float y = miny; y <= maxy; y++)
+            {
+                //Debug.Log("("+x+","+y+")");
+                Partition p = new Partition(new Vector2(x+0.5f, y+0.5f));
+
+                partitions[(int)(p.GetPosition().x + xStorageOffset)].Add(p);
+
+/*                 int storagex = (int)(p.GetPosition().x + xStorageOffset);
+                int storagey = (int)(p.GetPosition().y + yStorageOffset);
+
+                Debug.Log("[" + storagex + ", " + storagey
+                            + "]: " + partitions[storagex][storagey]); */
+                
+                // Add edges to existing nodes
+                if (x > minx)
+                {
+                    // Connect to left node
+                    AddEdge(p, GetPartition(x-0.5f, y+0.5f));
+
+                    if (y < maxy)
+                    {
+                        // Connect to upper-left node
+                        AddEdge(p, GetPartition(x-0.5f, y+1.5f));
+                    }
+                    if(y > miny)
+                    {
+                        // Connect to lower-left node
+                        AddEdge(p, GetPartition(x-0.5f, y-0.5f));
+                    }
+                }
+                if (y > miny)
+                {
+                    // Connect to lower node
+                    AddEdge(p, GetPartition(x+0.5f,y-0.5f));
+                }
+            }
+        }
+    }
+
+    // Create an edge between two nodes in the navmesh
+    void AddEdge(Partition a, Partition b)
+    {
+        Edge edge = new Edge(a,b);
+        a.GetEdges().Add(edge);
+        b.GetEdges().Add(edge);
+    }
+
+    Partition GetPartition(float x, float y)
+    {
+        //Debug.Log("x = "+(int)(x+xStorageOffset)+", y = "+(int)(y+yStorageOffset));
+        return partitions[(int)(x+xStorageOffset)][(int)(y+yStorageOffset)];
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+
+        float[] xs = {referencePlaneCorners[0].x, referencePlaneCorners[1].x};
+        float[] ys = {referencePlaneCorners[0].y, referencePlaneCorners[1].y};
+        
+        Gizmos.DrawLine(new Vector3(xs[0], height, ys[0]), new Vector3(xs[0], height, ys[1]));
+        Gizmos.DrawLine(new Vector3(xs[0], height, ys[1]), new Vector3(xs[1], height, ys[1]));
+        Gizmos.DrawLine(new Vector3(xs[1], height, ys[1]), new Vector3(xs[1], height, ys[0]));
+        Gizmos.DrawLine(new Vector3(xs[1], height, ys[0]), new Vector3(xs[0], height, ys[0]));
     }
 }
--- COMP 521 - A3 - Pathfinding/Assets/Scripts/Partition.cs
+++ COMP 521 - A3 - Pathfinding/Assets/Scripts/Partition.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,5 +20,46 @@ public class Partition
     public Vector2 GetPosition()
     {
         return position;
+    }
+
+    public List<Edge> GetEdges()
+    {
+        return edges;
+    }
+
+    public void Draw(Color partColor, Color edgeColor)
+    {
+        float[] xs = {position.x-0.5f, position.x+0.5f};
+        float[] ys = {position.y-0.5f, position.y+0.5f};
+        
+        Debug.DrawLine(new Vector3(xs[0], 0, ys[0]), new Vector3(xs[0], 0, ys[1]), partColor, 100f);
+        Debug.DrawLine(new Vector3(xs[0], 0, ys[1]), new Vector3(xs[1], 0, ys[1]), partColor, 100f);
+        Debug.DrawLine(new Vector3(xs[1], 0, ys[0]), new Vector3(xs[0], 0, ys[0]), partColor, 100f);
+        Debug.DrawLine(new Vector3(xs[1], 0, ys[1]), new Vector3(xs[1], 0, ys[0]), partColor, 100f);
+
+        foreach (Edge e in edges)
+        {
+            if(e.distance > 1)
+            {
+                if (e.a == this)
+                {
+                    if (e.b.position.x < position.x || e.b.position.y < position.y)
+                    {
+                        Debug.DrawLine(new Vector3(e.b.position.x, 0, e.b.position.y), new Vector3(e.a.position.x, 0, e.a.position.y), edgeColor, 100f);
+                    }
+                } else 
+                {
+                    if (e.a.position.x < position.x || e.a.position.y < position.y)
+                    {
+                        Debug.DrawLine(new Vector3(e.b.position.x, 0, e.b.position.y), new Vector3(e.a.position.x, 0, e.a.position.y), edgeColor, 100f);
+                    }
+                }
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Partition with pos: "+position.ToString();
     }
 }
